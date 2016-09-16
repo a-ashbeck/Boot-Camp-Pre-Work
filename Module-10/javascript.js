aa3fd52d591bccecad9374f320d7213546882214
Alan <a-ashbeck@northwestern.edu>
2016-09-16 08:25:05 -0500

    Add module 10 homework

---
+++ Module-10/javascript.js
@@ -0,0 +1,12 @@
+function grow(height, width) {
+     document.getElementById("box").style.height = height;
+     document.getElementById("box").style.width = width;
+}
+
+function colorChange(color) {
+     document.getElementById("box").style.backgroundColor = color;
+}
+
+function opacityChange(opacity) {
+     document.getElementById("box").style.opacity = opacity;
+}
